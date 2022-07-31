21af40255fa197070f1ece2c0bac9dc710271524
Chloe Scheffler <chloescheffler@gmail.com>
2022-07-31 10:28:50 -0400

    commit tree 466ce158d49e5059efaebdcc056bc5206fff2dcc

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function countDown() {
+    let countDown = 11;
+    while (countDown > 0) {
+        countDown--;
+        console.log(countDown);
+    }
+}
+
+const message = [];
+function writeCards(name, string) {
+    for (let i = 0; i < name.length; i++) {
+        message.push(`Thank you, ${name[i]}, for the wonderful ${string} gift!`);
+        console.log(message);
+    }
+    return message;
+}
